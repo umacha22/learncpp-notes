105f61a1161491e106ba548a9ad9e23ef9238321
uchaudhry <ucopper4@gmail.com>
2026-01-20 15:10:31 -0600

    ch7-wip

---
+++ ch7.cpp
@@ -0,0 +1,11 @@
+
+    // 
+// A compound statement (also called a block, or block statement) is a group of zero or more statements that is treated by the compiler as if it were a single statement.
+
+// namespace NamespaceIdentifier
+// {
+//     // content of namespace here
+// }
+
+// Global variables are created when the program starts (before main() begins execution), 
+// and destroyed when it ends. This is called static duration. Variables with static duration are sometimes called static variable
